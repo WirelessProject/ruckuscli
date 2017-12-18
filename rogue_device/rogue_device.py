c1c3c62e817c2e0885b6a30bc700794107f5f000
howard1337 <y60091534@gmail.com>
2017-12-18 17:15:26 +0800

    Parse valid SSIDs from ZD and Complete Mail function

--- rogue_device/rogue_device.py
+++ rogue_device/rogue_device.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
-
 import datetime
 import pexpect
 import getpass
 import re
 import smtplib
 
+username = ''
+password = ''
+fromaddr = ''
+toaddrs = ''
 
 def mailing(device):
 	server = smtplib.SMTP('smtp.gmail.com:587')
@@ -23,10 +26,6 @@ def mailing(device):
 		"SSID = %s" % (device['SSID']),
 		"Last_Detected = %s" %(device['Last_Detected']),
 	  ])
-	username = '******'
-	password = '******'
-	fromaddr = '******'
-	toaddrs = '******'
 	server.login(username,password)
 	server.sendmail(fromaddr, toaddrs, msg)
 	server.quit()
@@ -43,6 +42,20 @@ def main():
         print(datetime.datetime.now().isoformat(),
             'A privileged user is already logged in.')
         return
+    p.sendline('show wlan-group all')
+    p.expect('ruckus#')
+    data = p.before
+    data = data.split('WLAN Service:')[1:]
+    # get valid SSIDs
+    valid_SSID = []
+    for i in (data):
+        tmp = re.findall('NAME= (\S+)\r\r\n', i)[0]
+        if not tmp:
+            continue
+        valid_SSID.append(tmp)
+    
+    # valid_SSID.append('NTU')
+
     p.sendline('show rogue-devices')
     p.expect('Current Active Rogue Devices:\r\r\n')
     p.expect('ruckus#')
@@ -50,10 +63,10 @@ def main():
     entries = re.split('Rogue Devices:\r\r\n', data.decode('utf-8'))[1:]
     rogue_devices = []
     for entry in entries:
-    	#empty line
-    	if len(re.findall('Mac Address= (\S+)\r\r\n', entry)) == 0:
-    		continue
-    	Mac = re.findall('Mac Address= (\S+)\r\r\n', entry)[0]
+            #empty line
+        if len(re.findall('Mac Address= (\S+)\r\r\n', entry)) == 0:
+            continue
+        Mac = re.findall('Mac Address= (\S+)\r\r\n', entry)[0]
         Channel = re.findall('Channel= (\S+)\r\r\n',entry)[0]
         Radio = re.findall('Radio= (\S+)\r\r\n',entry)[0]
         Type = re.findall('Type= (\S+)\r\r\n',entry)[0]
@@ -61,34 +74,50 @@ def main():
      	#check for empty SSID
         if len(re.findall('SSID= (\S+)\r\r\n',entry)) != 0:
             if len(re.findall('SSID= (\S+ \S+)\r\r\n',entry)) == 0:
-            	SSID = re.findall('SSID= (\S+)\r\r\n',entry)[0]
+                SSID = re.findall('SSID= (\S+)\r\r\n',entry)[0]
             else:
-            	SSID = re.findall('SSID= (\S+ \S+)\r\r\n',entry)[0]
+                SSID = re.findall('SSID= (\S+ \S+)\r\r\n',entry)[0]
         else:
-	    	SSID = []
+            SSID = []
         Last_Detected = re.findall('Last Detected= (\S+ \S+)\r\r\n',entry)[0]
         rogue_devices.append({
-        		'Mac' : Mac,
-        		'Channel': Channel,
-        		'Radio' : Radio,
-        		'Type' : Type,
-        		'Encryption' : Encryption,
-        		'SSID' : SSID,
-        		'Last_Detected' : Last_Detected,
+                'Mac' : Mac,
+                'Channel': Channel,
+                'Radio' : Radio,
+                'Type' : Type,
+                'Encryption' : Encryption,
+                'SSID' : SSID,
+                'Last_Detected' : Last_Detected,
         	})
     for device in rogue_devices:
-    	if device['SSID'] == 'csie' or device['SSID'] == 'csie-5G' or device['SSID'] == 'NTU':
-    		mailing(device)
-    	print(device['SSID'])
+        print(device['SSID'])
+        # if device['SSID'] == 'csie' or device['SSID'] == 'csie-5G':
+        if device['SSID'] in valid_SSID :
+            mailing(device)
+        print(device['SSID'])
 
     p.sendline('exit')
     p.close()
 
 
 if __name__ == '__main__':
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    while True:
+        try:
+            username = raw_input('Please Enter your Gmail Account: ')
+            password = getpass.getpass('Password: ')
+            server.login(username,password)
+            break
+        except smtplib.SMTPException:
+            print("your username or password is incorrect, please try again") 
+            continue
+    fromaddr = username + '@gmail.com';
+    toaddrs = raw_input('Please Enter your Gmail toaddr: ')
     print(datetime.datetime.now().isoformat(), 'Start')
     try:
         main()
     except pexpect.TIMEOUT:
         print('Timeout.')
+    server.quit()
     print(datetime.datetime.now().isoformat(), 'End')
